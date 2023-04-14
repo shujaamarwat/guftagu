8630e99a7bf6f8c9d81c0be2810634da3abccd07
Shujaa Marwat <shujaa.khan@devsinc.com>
2023-04-14 16:51:19 +0500

    Add configuration for tailwind *postcss.config.js: Added PostCSS plugins. *src/App.js: Changed the view for now. *tailwind.config.js: Added configuration for tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App pt-[4vh]">
+      <h2 className="text-4xl font-bold mb-4">Guftagu</h2>
+    </div>
+  );
+}
+
+export default App;
