288ff6b7755e5e867d4baa3bf6f4c5650ee0f78e
Shujaa Marwat <shujaa.khan@devsinc.com>
2023-04-17 18:41:42 +0500

    Added Logout button and Updated the routes for authentification * src/App.js * Home/index.jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import './styles/app.css'
+import './styles/tailwind.css'
 import { Home } from './components/Home';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import { auth } from './config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Alert } from './components/Shared';
+
+const ProtectedRoute = ({ children }) => {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    <Alert message={"Please Login First"}/>
+    return <p>Loading...</p>;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace/>;
+  }
+
+  return children;
+};
 
 function App() {
   return (
     <div className="App pt-[4vh]">
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+            } />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
 
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,34 @@
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../config/firebase";
+
+const LogoutButton = () => {
+  const navigate = useNavigate();
+
+  const logoutAction = (e) => {
+    e.preventDefault()
+    console.log("Here")
+    // logout(e)
+    signOut(auth).then(function() {
+      navigate("/login")
+      console.log("Sign Out Successful")
+    }).catch(function(error) {
+      console.log("Sign Out NOT Successful: ", error)
+    });
+  };
+
+  return(
+    <button type="button" className="bg-red-500 text-white p-2 rounded-full text-2xl p-[0.5em]" onClick = {(e) => logoutAction(e)}>
+      Logout
+    </button>
+  );
+}
+
 export const Home = () => {
   return(
-    <h2 className="text-4xl font-bold mb-4">Guftagu</h2>
+    <div>
+      <LogoutButton/>
+      <h2 className="text-4xl font-bold mb-4">Guftagu</h2>
+    </div>
   );
 }
