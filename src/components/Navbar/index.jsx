4aec0b714ff4d0c99f4d6166224c0730bab0c52a
Shujaa Marwat <shujaamarwat@gmail.com>
2023-06-01 18:58:48 +0500

    Resolved several browser related warnings

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -23,8 +23,8 @@ const SidebarButton = () => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          clip-rule="evenodd"
-          fill-rule="evenodd"
+          clipRule="evenodd"
+          fillRule="evenodd"
           d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
         ></path>
       </svg>
@@ -72,9 +72,9 @@ const SidebarMenu = () => {
   };
 
   const FullMenu = () => {
-    return menuOptions.map((op) => {
+    return menuOptions.map((op, i) => {
       return (
-        <MenuOption heading={op.heading} icon={op.icon} action={op.action} />
+        <MenuOption heading={op.heading} icon={op.icon} action={op.action} key={i}/>
       );
     });
   };
