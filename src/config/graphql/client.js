9203ad7ef26ebdd5a7667086553f7b48e17efd73
Shujaa Marwat <shujaamarwat@gmail.com>
2023-06-01 19:04:14 +0500

    Implemented GraphQL functionality: custom hooks for messages, Apollo Client configuration, and query/mutation definitions * hooks.js: Added custom hooks for adding messages and retrieving messages. * client.js: Configured Apollo Client for HTTP and WebSocket communication. * queries.js: Defined GraphQL queries and mutations in queries.js.

---
+++ src/config/graphql/client.js
@@ -0,0 +1,31 @@
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { Kind, OperationTypeNode } from "graphql";
+import { createClient as createWsClient } from "graphql-ws";
+import { getAccessToken } from "../auth";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:9000/graphql",
+});
+
+const wsLink = new GraphQLWsLink(
+  createWsClient({
+    url: "ws://localhost:9000/graphql",
+    connectionParams: () => ({ accessToken: getAccessToken() }),
+  })
+);
+
+function isSubscription({ query }) {
+  const definition = getMainDefinition(query);
+  return (
+    definition.kind === Kind.OPERATION_DEFINITION &&
+    definition.operation === OperationTypeNode.SUBSCRIPTION
+  );
+}
+export const client = new ApolloClient({
+  link: split(isSubscription, wsLink, httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default client;
